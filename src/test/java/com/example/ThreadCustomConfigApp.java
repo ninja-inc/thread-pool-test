1aab6f0f3a21e93221f02baa36e0cf1c5721e79e
takaaki.iida <takiida0605@gmail.com>
2017-05-01 16:25:41 +0900

    modify

--- src/test/java/com/example/ThreadCustomConfigApp.java
+++ src/test/java/com/example/ThreadCustomConfigApp.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+@EnableAsync
 @SpringBootApplication
 public class ThreadCustomConfigApp {
 
@@ -18,18 +18,18 @@ public class ThreadCustomConfigApp {
 		SpringApplication.run(ThreadPoolTestApplication.class, args);
 	}
 	
-	// This executor instance will be used by both @Aync & TaskExecutor
+	// This executor instance will be used by both @Aync & TaskExecutor if AsyncConfigurerSupport works
 	@Bean
 	public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(10);
+		executor.setCorePoolSize(3);
 		executor.initialize();
 		return executor;
 	}
 }
 
-@Configuration
-@EnableAsync
+//@Configuration
+//@EnableAsync
 class AsyncConfigurer extends AsyncConfigurerSupport {
 
     @Autowired
